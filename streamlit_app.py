a6bd82d15d598f7cda224ade36b0f7b94c1dd07e
Ashik Sathiya <92455762+AshikSathiya@users.noreply.github.com>
2023-11-25 18:06:25 -0800

    Sisplay results in DataFrame

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,7 +29,7 @@ streamlit.dataframe(fruits_to_show)
 #New Section to Display FruityVice API Response
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json())
+streamlit.text(fruityvice_response.json()) #Just writes data on Screen
 
 # normalizes json response
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
